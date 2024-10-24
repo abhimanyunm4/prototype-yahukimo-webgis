a69dd667627085c67bd8eb157f5d8ba089b50fa2
abhimanyunm4 <113840788+abhimanyunm4@users.noreply.github.com>
2024-10-24 14:28:19 +0700

    Add files via upload

---
+++ data/BandaraDekai_4.js
@@ -0,0 +1,1 @@
+var json_BandaraDekai_4 = {"type":"FeatureCollection","name":"BandaraDekai_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Nama":"Bandara Dekai"},"geometry":{"type":"Point","coordinates":[139.481330111283768,-4.855668954224304]}}]}
